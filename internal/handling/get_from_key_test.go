8dbd265a05a1792704ff5a517cfa41c7ddf93286
Ian Beauregard <ian.a.beauregard@pm.me>
2022-07-06 18:06:39 -0400

    Add unit tests for GET handler

---
+++ internal/handling/get_from_key_test.go
@@ -0,0 +1,55 @@
+package handling
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleGetFromKey(t *testing.T) {
+	assert.NotNil(t, HandleGetFromKey(&concreteRepoProxy{}))
+}
+
+type repoProxyMock struct {
+	RepoProxy
+	expectedUrl         string
+	expectedFoundStatus bool
+}
+
+func (m *repoProxyMock) getLongUrl(_ string) (string, bool) {
+	return m.expectedUrl, m.expectedFoundStatus
+}
+
+func TestHandleFound(t *testing.T) {
+	dummyLongUrl := "http://foobar.com"
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.Request = &http.Request{
+		Method: "GET",
+	}
+	ctx.AddParam("key", "my_key")
+	mock := &repoProxyMock{
+		expectedUrl:         dummyLongUrl,
+		expectedFoundStatus: true,
+	}
+	handle(ctx, mock)
+	assert.EqualValues(t, http.StatusMovedPermanently, w.Code)
+	assert.EqualValues(t, []string{dummyLongUrl}, w.Result().Header["Location"])
+}
+
+func TestHandleNotFound(t *testing.T) {
+	dummyLongUrl := ""
+	mock := &repoProxyMock{
+		expectedUrl:         dummyLongUrl,
+		expectedFoundStatus: false,
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/my_key_not_found", nil)
+	r := gin.Default()
+	r.GET("/:key", HandleGetFromKey(mock))
+	r.LoadHTMLFiles("../templates/not_found.html")
+	r.ServeHTTP(w, req)
+	assert.EqualValues(t, http.StatusNotFound, w.Code)
+}
