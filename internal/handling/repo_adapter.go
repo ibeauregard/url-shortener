b4c7b2fbeeedefcaab62ac26d9c3f0324a96bf77
Ian Beauregard <ian.a.beauregard@pm.me>
2022-07-28 18:50:08 -0400

    For repoAdapter, use interface embedding instead of composition

--- internal/handling/repo_adapter.go
+++ internal/handling/repo_adapter.go
@@ -14,40 +14,40 @@ type Repository interface {
 }
 
 type repoAdapter struct {
-	r Repository
+	Repository
 }
 
 func NewRepoAdapter(r Repository) *repoAdapter {
-	return &repoAdapter{r: r}
+	return &repoAdapter{r}
 }
 
-func (adapter *repoAdapter) Close() error {
-	err := adapter.r.Close()
+func (r *repoAdapter) Close() error {
+	err := r.Repository.Close()
 	if err != nil {
 		return fmt.Errorf("handling.Close: %w", err)
 	}
 	return nil
 }
 
-func (adapter *repoAdapter) getKey(longUrl string) (key string, found bool) {
-	mapping, err := adapter.r.FindByLongUrl(longUrl)
+func (r *repoAdapter) getKey(longUrl string) (key string, found bool) {
+	mapping, err := r.FindByLongUrl(longUrl)
 	if err != nil {
 		return "", false
 	}
 	return mapping.Key, true
 }
 
-func (adapter *repoAdapter) getLongUrl(key string) (longUrl string, found bool) {
-	mapping, err := adapter.r.FindByKey(key)
+func (r *repoAdapter) getLongUrl(key string) (longUrl string, found bool) {
+	mapping, err := r.FindByKey(key)
 	if err != nil {
 		return "", false
 	}
 	return mapping.LongUrl, true
 }
 
-func (adapter *repoAdapter) addMapping(longUrl string) (shortUrl string, err error) {
-	key := generateKey(longUrl, adapter.getNextDatabaseId())
-	err = adapter.r.Create(&repo.MappingModel{
+func (r *repoAdapter) addMapping(longUrl string) (shortUrl string, err error) {
+	key := generateKey(longUrl, r.getNextDatabaseId())
+	err = r.Create(&repo.MappingModel{
 		Key:     key,
 		LongUrl: longUrl,
 	})
@@ -57,6 +57,6 @@ func (adapter *repoAdapter) addMapping(longUrl string) (shortUrl string, err err
 	return key, nil
 }
 
-func (adapter *repoAdapter) getNextDatabaseId() uint {
-	return adapter.r.GetLastId() + 1
+func (r *repoAdapter) getNextDatabaseId() uint {
+	return r.GetLastId() + 1
 }
